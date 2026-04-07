f5afb74f9e0d9efdc6bf63b27e74051ac06ef466
sourpunchlover <sndgpdesigns@gmail.com>
2026-04-07 15:31:38 -0400

    Added three different salaries

---
+++ src/main/java/com/pluralsight/MathApp.java
@@ -0,0 +1,10 @@
+package com.pluralsight;
+
+public class MathApp {
+    public static void main(String[] args) {
+        //Question 1:
+        long bobSalary = 140000;
+        long garySalary = 165000;
+        long highestSalary = 200000;
+    }
+}
