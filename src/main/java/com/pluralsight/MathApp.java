10fc80697034aef84d2daaa2663b5b7eed9a54d0
sourpunchlover <sndgpdesigns@gmail.com>
2026-04-08 09:58:02 -0400

    Added additional sout lines to question 1 explaining what each variable is for. Also added empty sout lines to questions 1-3 to create space between answers.

--- src/main/java/com/pluralsight/MathApp.java
+++ src/main/java/com/pluralsight/MathApp.java
@@ -4,15 +4,30 @@ public class MathApp {
     public static void main(String[] args) {
         //Question 1:
         long bobSalary = 140000L;
+        System.out.println("Bob's salary is" + " " + bobSalary + ".");
         long garySalary = 165000L;
+        System.out.println("Gary's salary is" + " " + garySalary + ".");
         long highestSalary = 165000L;
-        System.out.println(Math.max(bobSalary, garySalary));
+        System.out.println("The highest salary is" + " " + highestSalary + ".");
+        System.out.println("Gary has the highest salary" + " " + Math.max(bobSalary, garySalary) + ".");
+        System.out.println();
 
         //Question 2:
-        //will come back to this one
+        Long carPrice = 20000L;
+        Long truckPrice = 50000L;
+        System.out.println("The price of a car is" + " " + carPrice + ".");
+        System.out.println("The price of a truck is" + " " + truckPrice + ".");
+        System.out.println();
 
         //Question 3:
         double radius = 7.25;
+        double area = Math.PI * radius * radius;
+        System.out.println("The area of a circle when radius is given is" + " " + area + ".");
+        System.out.println();
+
+        //Question 4:
+        double some_random_num = 5.0;
+
 
 
 
